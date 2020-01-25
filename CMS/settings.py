f8623abd7a17ee81a5523dd794ee8d4da61dd9ad
gokul <gokulkrh@gmail.com>
2020-01-25 21:37:00 +0530

    edited homepage

--- CMS/settings.py
+++ CMS/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'couriermanage.apps.CouriermanageConfig',
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
