44737d9ee52a74d9fc8f7d2bedb47714c8b14d73
gokul <gokulkrh@gmail.com>
2020-01-26 08:09:34 +0530

    implemented new user registration

--- CMS/urls.py
+++ CMS/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('couriermanage.urls'), name='home'),
-
+    path('register/', user_views.register),
+    path('', include('couriermanage.urls')),
 ]
