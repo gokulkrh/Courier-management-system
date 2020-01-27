919f748bddd592d55567b526e6c2f37b64834232
gokul <gokulkrh@gmail.com>
2020-01-27 23:14:32 +0530

    implemented user login

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-	email = forms.EmailField()
+	Amrita_mail = forms.EmailField()
 
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+		fields = ['username', 'Amrita_mail', 'password1', 'password2']
