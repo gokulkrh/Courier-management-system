baa525113568ca4156540ed7142ef9c48b20a349
gokul <gokulkrh@gmail.com>
2020-01-29 01:09:59 +0530

    design changes

--- couriermanage/views.py
+++ couriermanage/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def home(request):
 	return render(request, 'couriermanage/home.html', {'title': 'home'})
 
+@login_required
 def main(request):
 	return render(request, 'couriermanage/main.html')
