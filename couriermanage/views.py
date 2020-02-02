19e9e8137d2ad91c748c526ff18642be69618a1e
gokul <gokulkrh@gmail.com>
2020-02-03 03:12:36 +0530

    created users page

--- couriermanage/views.py
+++ couriermanage/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Courier
 
 def home(request):
 	return render(request, 'couriermanage/home.html', {'title': 'home'})
 
 @login_required
 def main(request):
-	return render(request, 'couriermanage/main.html', {'title': 'main'})
+	couriers = Courier.objects.all()
+	return render(request, 'couriermanage/main.html', {'couriers': couriers})
