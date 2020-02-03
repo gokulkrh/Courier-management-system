68e6ef9cf350b3c87b588f5963aac7942c696082
gokul <gokulkrh@gmail.com>
2020-02-03 22:07:56 +0530

    implemented search

--- couriermanage/views.py
+++ couriermanage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .models import Courier
 
@@ -8,5 +9,25 @@ def home(request):
 
 @login_required
 def main(request):
-	couriers = Courier.objects.all()
+	context = {}
+	query = ""
+	if request.GET:
+		query = request.GET['q']
+		context['query'] = str(query)
+	couriers = search(query)
 	return render(request, 'couriermanage/main.html', {'couriers': couriers})
+
+
+def search(query=None):
+	queryset = []
+	queries = query.split(" ")
+	for q in queries:
+		couriers = Courier.objects.filter(
+			Q(student_rollno__icontains=q) |
+			Q(date_recieved__icontains=q) |
+			Q(service__icontains=q)
+		)
+		for courier in couriers:
+			queryset.append(courier)
+
+	return list(set(queryset))
--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-	Amrita_mail = forms.EmailField()
+	email = forms.EmailField()
 
 	class Meta:
 		model = User
-		fields = ['username', 'Amrita_mail', 'password1', 'password2']
+		fields = ['username', 'email', 'password1', 'password2']
