813d149bfaddc5a3d853131d8924d226347d1522
gokul <gokulkrh@gmail.com>
2020-02-08 05:25:53 +0530

    modified admin page

--- couriermanage/admin.py
+++ couriermanage/admin.py
@@ -3,7 +3,9 @@ from .models import Courier
 
 
 class CourierAdmin(admin.ModelAdmin):
-	list_display = ('name', 'student_rollno', 'date_recieved', 'service')
+	list_display = ('id', 'name', 'student_rollno', 'date_recieved', 'service')
+	change_list_template = 'admin/change_list.html'
+	actions = None
 
 admin.site.site_header = 'ACMS administration'
 admin.site.site_title = 'ACMS-admin'
